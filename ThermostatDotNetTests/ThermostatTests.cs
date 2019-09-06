8f0841af04f7319d0c8ea37c2bf62c1d3e6fa48d
Jordan Roberts <jordanroberts@talk21.com>
2019-09-06 18:03:11 +0100

    Refactored tests a little

--- ThermostatDotNetTests/ThermostatTests.cs
+++ ThermostatDotNetTests/ThermostatTests.cs
@@ -2,82 +2,76 @@ using System.Web.Mvc;
 using NUnit.Framework;
 using ThermostatDotNet.Controllers;
 
-namespace ThermostatTests
-{
-    public class ThermostatTests
-    {
+namespace ThermostatTests {
 
-        [Test]
-        public void ReturnsCurrentTemperature()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            int actual = thermostat.GetTemp();
-            int expected = 20;
-            Assert.AreEqual(expected, actual);
+    [TestFixture]
+    public class ThermostatTests {
+
+        private ThermostatController thermostat;
+
+        [SetUp]
+        public void Init () {
+            thermostat = new ThermostatController ();
+            thermostat.Reset ();
+
         }
 
         [Test]
-        public void ResetsTemperatureOnThermostat()
-        {
-            var thermostat = new ThermostatController();
-            int actual = thermostat.Reset();
+        public void ReturnsCurrentTemperature () {
+
+            int actual = thermostat.GetTemp ();
             int expected = 20;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
 
         [Test]
-        public void IncreasesTempByOne()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            int actual = thermostat.Increase(20);
+        public void ResetsTemperatureOnThermostat () {
+            int actual = thermostat.Reset ();
+            int expected = 20;
+            Assert.AreEqual (expected, actual);
+        }
+
+        [Test]
+        public void IncreasesTempByOne () {
+
+            int actual = thermostat.Increase (20);
             int expected = 21;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
 
         [Test]
-        public void DecreasesTempByOne()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            int actual = thermostat.Decrease(20);
+        public void DecreasesTempByOne () {
+
+            int actual = thermostat.Decrease (20);
             int expected = 19;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
 
         [Test]
-        public void DecreasesAndIncreases()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            var temp1 = thermostat.Decrease(20);
-            var temp2 =thermostat.Decrease(temp1);
-            var temp3 = thermostat.Increase(temp2);
-            int actual = thermostat.Decrease(temp3);
+        public void DecreasesAndIncreases () {
+
+            var temp1 = thermostat.Decrease (20);
+            var temp2 = thermostat.Decrease (temp1);
+            var temp3 = thermostat.Increase (temp2);
+            int actual = thermostat.Decrease (temp3);
             int expected = 18;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
 
         [Test]
-        public void WillNotExceedMaximumTemperature()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            int actual = thermostat.Increase(30);
+        public void WillNotExceedMaximumTemperature () {
+
+            int actual = thermostat.Increase (30);
             int expected = 30;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
 
-
         [Test]
-        public void WillNotGoBelowMinTemperature()
-        {
-            var thermostat = new ThermostatController();
-            thermostat.Reset();
-            int actual = thermostat.Decrease(10);
+        public void WillNotGoBelowMinTemperature () {
+
+            int actual = thermostat.Decrease (10);
             int expected = 10;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual (expected, actual);
         }
     }
 }
